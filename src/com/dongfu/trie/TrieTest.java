2a1ce2ff7d17fb8a27770224efd470ab885ac2a4
dongfu <1239927525@qq.com>
2017-01-08 21:58:47 +0800

    Change the test method, main().

--- src/com/dongfu/trie/TrieTest.java
+++ src/com/dongfu/trie/TrieTest.java
@@ -12,24 +12,42 @@ public class TrieTest {
         TrieTreeWithList tt = new TrieTreeWithList();
 //        TrieTree tt = new TrieTree();
         LinkedList<String> list = new LinkedList<>();
+//        LinkedList<Word> words = new LinkedList<>();
+
         list.add("app");
-        Word w1 = new Word("app");
-        Word w2 = new Word("apparent");
-        Word w3 = new Word("apart");
-        Word w4 = new Word("abandon");
-        Word w5 = new Word("bachelor");
+        list.add("apparent");
+        list.add("apart");
+        list.add("abandon");
+        list.add("bachelor");
 
-        tt.insert(w1);
-        tt.insert(w2);
-        tt.insert(w3);
-        tt.insert(w4);
-        tt.insert(w5);
+        for (String word:list) {
+            Word w = new Word(word);
+//            words.add(w);
+            tt.insert(w);
+        }
 
-        System.out.println(tt.search("app").getWord().getWord());
-        System.out.println(tt.search("apparent").getWord().getWord());
-        System.out.println(tt.search("apart").getWord().getWord());
-        System.out.println(tt.search("abandon").getWord().getWord());
-        System.out.println(tt.search("bachelor").getWord().getWord());
+//        Word w1 = new Word("app");
+//        Word w2 = new Word("apparent");
+//        Word w3 = new Word("apart");
+//        Word w4 = new Word("abandon");
+//        Word w5 = new Word("bachelor");
+//
+//        tt.insert(w1);
+//        tt.insert(w2);
+//        tt.insert(w3);
+//        tt.insert(w4);
+//        tt.insert(w5);
+
+        for (String word:list) {
+            System.out.println(tt.search(word).getWord().getWord());
+        }
+
+//        System.out.println(tt.search("app").getWord().getWord());
+//        System.out.println(tt.search("apparent").getWord().getWord());
+//        System.out.println(tt.search("apart").getWord().getWord());
+//        System.out.println(tt.search("abandon").getWord().getWord());
+//        System.out.println(tt.search("bachelor").getWord().getWord());
+
         System.out.println(tt.getWordCount());
         tt.delete("bachelor");
         System.out.println(tt.getWordCount());
